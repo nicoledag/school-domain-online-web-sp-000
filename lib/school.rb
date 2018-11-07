ca963c4b005bf1ae280691647be970ad63bd460e
Nicole D <nnardella@gmail.com>
2018-11-07 17:09:26 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
-# code here!
+require 'pry'
+
+class School
+
+  attr_accessor 
+  attr_reader :name, :grade, :sort
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+         if @roster[grade]
+        @roster[grade] << name
+
+       else
+         @roster[grade] = [name]
+
+      end
+    end
+
+end
+
+
+def grade(grade)
+
+
+end
+
+def sort
+
+
+end
