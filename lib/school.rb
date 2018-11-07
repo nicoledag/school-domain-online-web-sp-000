92709cdf10e601a56eeccbc395aba2a0e04633e2
Nicole D <nnardella@gmail.com>
2018-11-07 17:25:51 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,8 +2,8 @@ require 'pry'
 
 class School
 
-  attr_accessor :grade
-  attr_reader :name, :roster, :sort
+  attr_accessor
+  attr_reader :name, :roster, :grade, :sort
 
   def initialize(name)
     @name = name
